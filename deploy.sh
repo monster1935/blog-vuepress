57bfe6840c79750d325f37f72069761463a9d392
monster1935 <946815480@qq.com>
2022-01-18 22:56:05 +0800

    chore: 修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:monster1935/blog-vuepress.git main:gh-pages
+git push -f https://github.com/monster1935/blog-vuepress.git master:gh-pages
 
 cd -
